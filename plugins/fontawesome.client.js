f78ededb3182d99c0d1b75e595e04d771f92631a
Maximilien Pelletier <maximilienpelletier@gmail.com>
2025-09-21 17:00:22 +0200

    problem with Vercel Deploy fontawesome.client.js

--- plugins/fontawesome.client.js
+++ plugins/fontawesome.client.js
@@ -1,7 +1,7 @@
-import { library } from 'fortawesome/fontawesome-svg-core'
-import { fab } from 'fortawesome/free-brands-svg-icons'
-import { fas } from 'fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from 'fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default defineNuxtPlugin((nuxtApp) => {
   library.add(fab, fas)
